10eafff9be64f6e74344c31d610c8e87caffae3d
Engr Arslan Amanat <78694077+arsalanamanat@users.noreply.github.com>
2021-07-13 19:13:02 +0200

    Completed all the CRUD Functions. Can also show and read contect of all blogs

--- week2/homework/exercises/1-blog-API/CRUD Functions/readAllPost.js
+++ week2/homework/exercises/1-blog-API/CRUD Functions/readAllPost.js
@@ -2,9 +2,24 @@ const fs = require('fs');
 const path = require('path');
 
 const readAllPost = (req, res) => {
-  const files = fs.readdirSync(path.join(__dirname, '../Created Files'));
-  res.status(200);
-  res.send(files);
+  const blogs = fs.readdirSync(path.join(__dirname, '../Created Files'));
+  const data = [];
+
+  if (blogs.length > 0) {
+    blogs.forEach((blog) => {
+      const content = fs
+        .readFileSync(path.join(__dirname, '../Created Files', blog))
+        .toString('utf-8');
+
+      data.push(`{${blog} : ${content}}`);
+    });
+
+    res.status(200);
+    res.send(data);
+  } else {
+    res.status(500);
+    res.send(`Sorry We do not have any Blogs`);
+  }
 };
 
 module.exports = readAllPost;
