c253fe413da1df9e68841e8270d58cfc2012915d
Engr Arslan Amanat <78694077+arsalanamanat@users.noreply.github.com>
2021-07-17 12:46:48 +0200

    Completed Ex 2

--- week3/homework/exercises/2-authentication/script.js
+++ week3/homework/exercises/2-authentication/script.js
@@ -1,15 +1,35 @@
-
 /**
  * 2. Authentication
- * 
+ *
  * Using node-fetch make an authenticated request to https://restapiabasicauthe-sandbox.mxapps.io/api/books
  * Print the response to the console. Use async-await and try/catch.
- * 
+ *
  * Hints:
  * - for basic authentication the username and password need to be base64 encoded
  */
 function printBooks() {
   // YOUR CODE GOES IN HERE
+  const express = require('express');
+  const app = express();
+  const fetch = require('node-fetch');
+
+  try {
+    app.get('/', async (req, res) => {
+      const api_Url = 'https://restapiabasicauthe-sandbox.mxapps.io/api/books';
+      const apiData = await fetch(api_Url, {
+        headers: { Authorization: 'Basic YWRtaW46aHZnWDhLbFZFYQ==' },
+      });
+
+      const jsonData = await apiData.json();
+      res.status(200);
+      console.log(jsonData);
+    });
+  } catch (err) {
+    res.status(404);
+    res.send(`Invalid Request`);
+  }
+
+  app.listen(3000);
 }
 
 printBooks();
